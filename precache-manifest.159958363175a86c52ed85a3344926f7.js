5a031abebf7ffb3c43766c56a75d240ff06cc5c6
Bernt Anker <bernt.anker@me.com>
2020-01-15 21:15:24 +0100

    Updates

---
+++ precache-manifest.159958363175a86c52ed85a3344926f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a58d12107fc4fb0234d274156e65e1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa13aa40a917855c8ec7",
+    "url": "/monsters-rolodex/static/css/main.94cbd275.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa13aa40a917855c8ec7",
+    "url": "/monsters-rolodex/static/js/main.3f07d52d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
